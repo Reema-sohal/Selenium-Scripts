eaf5bba17991169410597f783f99a7f9c1d7bbca
Reema-sohal <reemasohal30@gmail.com>
2019-09-16 19:54:47 +0530

    Login VAlidation Change

--- github_signin.rb
+++ github_signin.rb
@@ -1,25 +1,42 @@
 require "selenium-webdriver"
+require "pry"
+require "pry-byebug"
 
 driver = Selenium::WebDriver.for :chrome
+driver.manage.timeouts.page_load = 120
 driver.navigate.to "http://github.com"
 print "website loaded \n"
 
-element = driver.find_element(css: "body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3")
 print "finding sign-in button \n"
-element.click
+signin = driver.find_element(css: "body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3")
+signin.click
 print "clicked on sign-inbutton"
 
 
-element = driver.find_element(name: "login")
-element.send_keys "Reema-sohal"
+username = driver.find_element(name: "login")
+username.send_keys "Reema-sohal"
 print "username entered \n"
 
-element = driver.find_element(name: "password")
+password = driver.find_element(name: "password")
 print "Please add password \n"
-password = gets
+passwordin = gets.strip
+password.send_keys(passwordin)
 
-element = driver.find_element(name: "commit")
-element.click
+submitbutton = driver.find_element(name: "commit")
+submitbutton.click
 print "clicked on sign-in \n"
+
+# Check the login was successful
+# binding.pry
+begin 
+	see_avatar = driver.find_element(class: "avatar")
+	print "clicked on avatar \n"
+	loginsuccess = driver.find_element(css: "body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details > details-menu > div.header-nav-current-user.css-truncate > a")
+	print "Login Successful \n"
+rescue
+	print "Password Incorrect \n" 
+	print "Login unsuccessful \n"
+end	
+sleep 2
 
 driver.quit 
